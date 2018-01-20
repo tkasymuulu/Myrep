577a5b29de834173276e4ef0f083136d2e335fd5
tkasymuulu <tkasymuulu89@gmail.com>
2018-01-21 00:12:22 +0600

    add new

---
+++ DEMO_boolean/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        boolean b;
+        b=false;
+
+            if (b=false)
+        System.out.println(b);
+
+
+    }
+}
---
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -0,0 +1,13 @@
+
+package javaapplication17;
+
+public class JavaApplication17 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Hoho tarchki");
+        
+        
+    }
+    
+}
