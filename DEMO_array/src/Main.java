9397abcf72dec1b0efdaa52269719a7baff9743c
tkasymuulu <tkasymuulu89@gmail.com>
2018-01-31 17:41:47 +0600

    +

--- DEMO_array/src/Main.java
+++ DEMO_array/src/Main.java
@@ -1,7 +1,13 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        mo
+        int a=5;
+        int b=((3+4)*(100+48)*a);
+        byte c= (byte) b;
+
+        System.out.println(c);l
+        System.out.println(b);
 
     }
 }
--- DEMO_if_then_else/src/Main.java
+++ DEMO_if_then_else/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-       byte b = ();
-        b= (byte) (b*2);
 
-        System.out.println(b);
     }
 }
