5ea43cc9580dd1a65e38943b78a91627dd21501c
tkasymuulu <tkasymuulu89@gmail.com>
2018-02-02 19:16:17 +0300

    +

--- DEMO_if_then_else/src/Main.java
+++ DEMO_if_then_else/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int a,b;
+        a=5;
+        b=10;
+
+        if (a<b & b>a) System.out.println("True");
+        else {
+            System.out.println("False");
+        }
     }
 }
