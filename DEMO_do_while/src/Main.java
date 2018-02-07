227d79e28608b01bc84049dceaa40b2f486251ab
tkasymuulu <tkasymuulu89@gmail.com>
2018-02-07 15:26:09 +0600

    ++

---
+++ DEMO_do_while/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)  throws java.io.IOException {
+
+        int input;
+        input   = (int) System.in.read();
+
+        System.out.println(input);
+
+    }
+}
