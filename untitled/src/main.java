9c287c25378e056a282f8a01b64e6c4abb847620
tkasymuulu <tkasymuulu89@gmail.com>
2018-09-03 11:14:27 +0600

    +

--- untitled/src/main.java
+++ untitled/src/main.java
@@ -1,14 +1,7 @@
-class Tank {
-    int level;
-}
+
 
 public class main {
     public static void main(String[] args) {
-
-        int a=5,b=15;
-
-        System.out.println(a+b);
-
+        System.out.println("fgfgfg");
     }
 }
-
